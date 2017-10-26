1054926d840239244acda7f51953ce4640661a75
Timur Morozov <radist@desant.biz>
2017-10-26 21:58:45 +0300

    use table instead of div/div for rendering optimization

--- sender.js
+++ sender.js
@@ -18,12 +18,18 @@ const remove = (socket) => {
   sockets.delete(socket)
 }
 
+function getRandomInt (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min // The maximum is exclusive and the minimum is inclusive
+}
+
 function send () {
   const msg = new ArrayBuffer(100 * 50 * 4)
   const view = new Int32Array(msg)
   const cellsCount = view.length
   for (let i = 0; i < cellsCount; i++) {
-    view[i] = ~(Math.random() * 1000)
+    view[i] = getRandomInt(10, 31000)
   }
   for (const socket of sockets) {
     socket.emit('grid-data', msg)
